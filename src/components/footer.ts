fcd52bced99f523c40f749c9a41ec6c87351c4ae
brichniachraf <brichniachraf33@gmail.com>
2025-10-13 20:04:36 +0100

    Initial commit

---
+++ src/components/footer.ts
@@ -0,0 +1,68 @@
+// src/components/footer.ts
+
+export function createFooter(): string {
+  return `
+      <footer>
+        <div class="footer">
+            <div class="tab">
+                <div class="socials">
+                <img src="/public/Black_and_White_Modern_Streetwear_Sport_Logo-removebg-preview - Modifié.png" alt="">
+                <p>follow us</p>
+                <div class="icons">
+                    <img src="/public/facebook.png" alt="">
+                    <img src="/public/instagram.png" alt="">
+                    <img src="/public/tiktok-circle.png" alt="">
+                </div>
+                </div>
+            </div>
+            <div class="tab">
+                <div class="links">
+                <h1>Quick links</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>Referral Program</li>
+                    <li>Become an Affiliate</li>
+                    <li>Log in</li>
+                    <li>baldi in the workplace</li>
+                    <li>j'aime le sex</li>
+                    <li>j'aime le sex</li>
+                </ul>
+                </div>
+            </div>
+            <div class="tab">
+                <div class="links">
+                <h1>about us</h1>
+                <ul>
+                    <li>How it Works</li>
+                    <li>About Us</li>
+                    <li>Our Plans</li>
+                    <li>Meal Prep Delivery</li>
+                    <li>Low Carb Meal Plans</li>
+                    <li>Balanced Plans</li>
+                    <li>Vegan Plans</li>
+                    <li>za3ket azer</li>
+                </ul>
+                </div>
+            </div>
+            <div class="tab">
+                <div class="apps">
+                    <h1>download our apps</h1>
+                    <div>
+                        <button><img id="images" src="/public/GetItOnGooglePlay_Badge_Web_color_English.png" alt=""></button>
+                        <button><img id="images" src="/public/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917.svg" alt=""></button>
+                    </div>
+                   
+               </div>
+            </div>
+        </div>
+    </footer>
+  `;
+}
+
+export function renderFooter(targetSelector: string = '#footer'): void {
+  const footerElement = document.querySelector(targetSelector);
+  if (footerElement) {
+    footerElement.innerHTML = createFooter();
+  }
+}
